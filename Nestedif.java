de18ca185e4d0c2f476ce2455a0c7f1d64685d25
Valarmathi S <valarmathi.sakthivel99@gmail.com>
2026-01-05 18:48:20 +0530

    Add Nested if condition example in Java
    A simple Java code showing how nested if-statements work with multiple conditions.

---
+++ Nestedif.java
@@ -0,0 +1,16 @@
+class Nestedif{
+  public static void main(String[] args){
+    boolean kfc = true;
+    boolean chicken = true;
+    boolean pepsi = true;
+    if(kfc){
+      System.out.println("I will go only to kfc");
+      if(chicken){
+        System.out.println("Eating chicken");
+        if(pepsi){
+          System.out.println("Drinking pepsi");
+        }
+      }
+    }
+  }
+}
